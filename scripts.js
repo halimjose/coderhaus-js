2c09de026015e98ca0c5e2e8a87e625c7939a1c6
halimjose <55866780+halimjose@users.noreply.github.com>
2021-08-30 22:46:48 -0300

    holaholahola

---
+++ scripts.js
@@ -0,0 +1,4 @@
+let firstValue = prompt("Ingrese el primer valor de la suma:");
+let secondValue = prompt("Ingrese el segundo valor de la suma:");
+
+alert("El resultado de la suma sería: " + (parseInt(firstValue) + parseInt(secondValue)));
