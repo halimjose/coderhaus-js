5d35191e53782947fd96acf518453103d4b83b33
halimjose <55866780+halimjose@users.noreply.github.com>
2021-09-01 23:00:53 -0300

    Actualizado el codigo para el desafio

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 let firstValue = prompt("Ingrese el primer valor de la suma:");
 let secondValue = prompt("Ingrese el segundo valor de la suma:");
+let resultado = (parseInt(firstValue) + parseInt(secondValue));
 
-alert("El resultado de la suma sería: " + (parseInt(firstValue) + parseInt(secondValue)));
-console.log("El resultado de la suma sería: " + (parseInt(firstValue) + parseInt(secondValue)));
+if(resultado < 1000){
+    alert("El resultado de la suma sería: " + resultado + " y es menor a 1000");
+} else if (resultado === 1000) {
+    alert("El resultado de la suma sería: " + resultado + ". Justo el número que tenía en mente.");
+} else {
+    alert("El resultado de la suma sería: " + resultado + " y es mayor a 1000");
+}
